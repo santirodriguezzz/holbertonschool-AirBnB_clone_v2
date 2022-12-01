2c7a0bb581afeca70c6efba330c6d0c0ab266c28
santirodriguezzz <santiagorodri130898@gmail.com>
2022-12-01 14:56:21 -0300

    test

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Start web application
+"""
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/states_list')
+def states_list():
+    """Render template with states
+    """
+    path = '7-states_list.html'
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, sorted_states=sorted_states)
+
+
+@app.teardown_appcontext
+def app_teardown(arg=None):
+    """Clean
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
