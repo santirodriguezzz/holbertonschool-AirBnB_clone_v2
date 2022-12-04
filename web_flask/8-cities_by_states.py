e8fde1c79d8474aaeffbbb5e495d852933a4c57b
santirodriguezzz <santiagorodri130898@gmail.com>
2022-12-04 15:48:33 -0300

    test

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
-"""Start web application with two routings
-"""
+""" script that starts a Flask web application """
 
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states')
-def states_list():
-    """Render template with states
-    """
-    path = '8-cities_by_states.html'
-    states = storage.all(State)
-
-    return render_template(path, states=states)
+@app.route("/cities_by_states", strict_slashes=False)
+def statelist():
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', sta=sta)
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session
-    """
+def removesql(exit):
     storage.close()
 
-
-if __name__ == '__main__':
-    app.url_map.strict_slashes = False
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
