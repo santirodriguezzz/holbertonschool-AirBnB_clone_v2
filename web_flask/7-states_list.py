d99ed1322a2307d6492eec4215b4c38523c5bff8
santirodriguezzz <santiagorodri130898@gmail.com>
2022-12-04 13:19:55 -0300

    test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python3
-"""Start web application
-"""
+""" script that starts a Flask web app """
 
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-@app.route('/states_list')
-def states_list():
-    """Render template for states
-    """
-    states = storage.all(State)
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template('7-states_list.html', sorted_states=sorted_states)
+@app.route("/states_list", strict_slashes=False)
+def statelist():
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', sta=sta)
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean
-    """
+def removesql(exit):
     storage.close()
 
-
-if __name__ == '__main__':
-    app.url_map.strict_slashes = False
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
